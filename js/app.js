2ea7df8c2445499e4bc3f45c5dd274a5974d29cb
Imantha-Kodikara <imantha.l.kodikara@gmail.com>
2025-04-04 16:22:55 +0530

    Final app

---
+++ js/app.js
@@ -0,0 +1,68 @@
+let index = 0;
+let countriesListArray = [];
+
+function countriesList() {
+  let countriesList = document.getElementById("countriesList");
+  let body = "";
+  fetch("json/countries.json")
+    .then((res) => res.json())
+    .then((dataList) => {
+      countriesListArray = dataList;
+      dataList.forEach((element) => {
+        body += `<div class="col">
+                <div class="card shadow-sm">
+                    <img src="${element.flags.png}" alt="${element.flags.alt}">
+                    <h2 class="card-text">${element.name.common}</h2>
+                  <div class="card-body">
+                    <p class="card-text">Capital : ${element.capital}</p>
+                    <p class="card-text">Region  : ${element.region}</p>
+                    <div class="d-flex justify-content-between align-items-center">
+                      <div class="btn-group">
+                        <button type="button" onclick = "loadModal(${index++})" class="btn btn-primary" data-bs-toggle="modal" data-bs-target="#staticBackdrop">
+    	                more info-->
+                        </button>
+                      </div>
+                     
+                    </div>
+                  </div>
+                </div>
+              </div>`;
+      });
+      countriesList.innerHTML = body;
+    });
+}
+countriesList();
+
+function loadModal(index) {
+  let modalTitle = document.getElementById("staticBackdropLabel");
+  modalTitle.innerHTML = `<h2>${countriesListArray[index].name.common}</h2>`;
+  let totLanguages = "";
+  let languages = Object.values(countriesListArray[index].languages); //get an languages details in every country
+
+  for (let i = 0; i < languages.length; i++) {
+    totLanguages += languages[i] + " ";
+  }
+
+  let modalBody = document.getElementById("modal-body");
+  modalBody.innerHTML = `
+                        <img src="${countriesListArray[index].flags.png}" alt="">
+                        <p>Official Name : ${countriesListArray[index].name.official}</p>
+                        <p>Capital       : ${countriesListArray[index].capital}</p> 
+                        <p>Region        : ${countriesListArray[index].region}</p>
+                        <p>Sub Region    : ${countriesListArray[index].subregion}</p>
+                        <p>Languages     : ${totLanguages}</p>
+                        <p>Time-Zone     : ${countriesListArray[index].timezones}</p>
+                        <p>Start of Week     : ${countriesListArray[index].startOfWeek}</p>
+                        <p>Population     : ${countriesListArray[index].population}</p>
+                        <a href="${countriesListArray[index].maps.googleMaps}">Google Map</a>
+  `;
+}
+
+function loadCountryDetails() {
+  let countryName = document.getElementById("default-search").value;
+  fetch(`https://restcountries.com/v3.1/name/${countryName}`)
+    .then((res) => res.json())
+    .then((dataList) => {
+      console.log(dataList);
+    });
+}
